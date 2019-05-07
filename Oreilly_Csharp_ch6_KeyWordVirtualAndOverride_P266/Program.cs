8f9fafda0abcc3f0c50c5f20dd4a04735746686a
Peng <Peng@PENG-PC>
2019-05-07 10:45:14 +0800

    加入專案檔案。

---
+++ Oreilly_Csharp_ch6_KeyWordVirtualAndOverride_P266/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oreilly_Csharp_ch6_KeyWordVirtualAndOverride_P266
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
